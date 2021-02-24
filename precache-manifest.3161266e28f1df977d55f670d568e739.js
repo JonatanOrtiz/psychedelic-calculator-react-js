4424adbc2d98b96a2e7282ba7a8f9e9e2b03f7ce
Jonatan <jonataneduard@gmail.com>
2021-02-24 17:11:48 -0300

    Updates

---
+++ precache-manifest.3161266e28f1df977d55f670d568e739.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791eb9ee2cd72782e8d65b638a05cd7b",
+    "url": "/psychedelic-calculator-react-js/index.html"
+  },
+  {
+    "revision": "9b18387be731fbc73f89",
+    "url": "/psychedelic-calculator-react-js/static/css/main.966d66e4.chunk.css"
+  },
+  {
+    "revision": "29247f4121e5aca679d0",
+    "url": "/psychedelic-calculator-react-js/static/js/2.d7e6f0de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/psychedelic-calculator-react-js/static/js/2.d7e6f0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b18387be731fbc73f89",
+    "url": "/psychedelic-calculator-react-js/static/js/main.8198b8ca.chunk.js"
+  },
+  {
+    "revision": "46068bc70d83bb8b5625",
+    "url": "/psychedelic-calculator-react-js/static/js/runtime-main.0043b02b.js"
+  },
+  {
+    "revision": "464e1880d7d5476d06c450d2b1ea2539",
+    "url": "/psychedelic-calculator-react-js/static/media/bgImg.464e1880.jpg"
+  }
+]);
